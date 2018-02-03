c570b8334690e3bbeb8a2d125143a65989c5a4b5
Jetmate <antonoutkine@gmail.com>
2018-02-03 17:33:30 -0600

    Finalize tiledmap

--- source/TiledLevel.hx
+++ source/TiledLevel.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.addons.tile.FlxTileAnimation;
+import flixel.addons.editors.tiled.TiledTilePropertySet;
 import flixel.tile.FlxTile;
 import flixel.system.debug.watch.Tracker.TrackerProfile;
 import haxe.io.Path;
@@ -10,6 +12,8 @@ import flixel.addons.editors.tiled.TiledObject;
 import flixel.addons.editors.tiled.TiledLayer;
 import flixel.addons.editors.tiled.TiledTileLayer;
 import flixel.addons.editors.tiled.TiledObjectLayer;
+import flixel.addons.editors.tiled.TiledTile;
+
 import flixel.addons.tile.FlxTileSpecial;
 import flixel.addons.tile.FlxTilemapExt;
 import flixel.tile.FlxBaseTilemap;
@@ -29,15 +33,44 @@ class TiledLevel extends TiledMap {
 	static var FLIPPED_XY_FLAG   = 0x20000000;
 
     static function flippedX(tile) {
-       return tile & FLIPPED_X_FLAG != 0;
+        return tile & FLIPPED_X_FLAG != 0;
     }
 
     static function flippedY(tile) {
-       return tile & FLIPPED_Y_FLAG != 0;
+        return tile & FLIPPED_Y_FLAG != 0;
     }
 
     static function flippedXY(tile) {
-       return tile & FLIPPED_XY_FLAG != 0;
+        return tile & FLIPPED_XY_FLAG != 0;
+    }
+
+    static function clearFlags(tile) {
+        return tile & ~(FLIPPED_X_FLAG |
+                        FLIPPED_Y_FLAG |
+                        FLIPPED_XY_FLAG);
+    }
+
+    static function getAnimation(tile, tileSet:TiledTileSet) {
+        if (tileSet.getPropertiesByGid(tile) != null) {
+            return cast(tileSet.getPropertiesByGid(tile), TiledTilePropertySet).animationFrames;
+        }
+        return null;
+    }
+
+    static function processAnimation(animation:Array<TileAnimationData>, name:String, looped:Bool) {
+        var smallest = Lambda.fold(animation, function (val:TileAnimationData, acc:Float) {
+            return (acc != 0 && acc < val.duration) ? acc : val.duration;
+        }, 0);
+
+        var frames = new Array<Int>();
+        for (data in animation) {
+            for (i in 0...(Std.int(data.duration / smallest))) {
+                frames.push(data.tileID + 1);
+            }
+        }
+
+        trace(frames, smallest);
+        return new FlxTileAnimation(name, frames, 1 / (smallest / 1000), looped);
     }
 
     public function new(tiledLevel:String) {
@@ -60,31 +93,38 @@ class TiledLevel extends TiledMap {
         for (layer in layers) {
             if (layer.type == TiledLayerType.TILE) {
                 var tileLayer = cast(layer, TiledTileLayer);
-
                 var tiles = new Array<Int>();
                 var specialTiles = new Array<FlxTileSpecial>();
 
+                // trace(tileLayer.tileArray);
                 for (i in 0...tileLayer.tileArray.length) {
                     var tile = tileLayer.tileArray[i];
-                    if (flippedX(tile) || flippedY(tile) || flippedXY(tile)) {
+                    var clearedTile = clearFlags(tile);
+                    // trace(tile, clearedTile);
+                    var animation = getAnimation(clearedTile, tileSet);
+
+                    if (flippedX(tile) || flippedY(tile) || flippedXY(tile) || animation != null && animation.length != 0) {
                         // var rotation = 0;
                         // if (flippedXY(tile)) {
                         //     if (flippedX(tile)) {
                         //         rotation = -90
                         //     }
                         // }
-                        specialTiles.push(new FlxTileSpecial(
-                            i,
+                        trace(clearedTile);
+                        var specialTile = new FlxTileSpecial(
+                            clearedTile,
                             flippedX(tile),
                             flippedY(tile),
                             flippedXY(tile) ? 90 : 0
-                        ));
+                        );
+                        if (animation != null && animation.length != 0) {
+                            specialTile.animation = processAnimation(animation, Std.string(clearedTile), true);
+                        }
+                        specialTiles.push(specialTile);
+                    } else {
+                        specialTiles.push(null);
                     }
-                    tiles.push(tile & ~(
-                        FLIPPED_X_FLAG |
-                        FLIPPED_Y_FLAG |
-                        FLIPPED_XY_FLAG
-                    ));
+                    tiles.push(clearedTile);
                 }
 
                 var tilemap = new FlxTilemapExt();
@@ -96,7 +136,7 @@ class TiledLevel extends TiledMap {
                     FlxTilemapAutoTiling.OFF,
                     tileSet.firstGID
                 );
-                trace(specialTiles);
+                trace(tiles);
                 tilemap.setSpecialTiles(specialTiles);
 
                 if (layer.name == "background") {
